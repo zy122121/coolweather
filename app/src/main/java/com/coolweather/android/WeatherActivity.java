b1d5efb2ead1cf50994e796649ce09e0ae21dd17
zengyong <1728018213@qq.com>
2018-02-02 10:52:03 +0800

    增加后台自动更新天气的功能

--- app/src/main/java/com/coolweather/android/WeatherActivity.java
+++ app/src/main/java/com/coolweather/android/WeatherActivity.java
@@ -1,5 +1,6 @@
 package com.coolweather.android;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.os.Build;
@@ -21,6 +22,7 @@ import android.widget.Toast;
 import com.bumptech.glide.Glide;
 import com.coolweather.android.gson.Forecast;
 import com.coolweather.android.gson.Weather;
+import com.coolweather.android.service.AutoUpdateService;
 import com.coolweather.android.util.HttpUtil;
 import com.coolweather.android.util.Utility;
 
@@ -109,40 +111,40 @@ public class WeatherActivity extends AppCompatActivity {
     public void requestWeather(final String weatherId){
         String weatherUrl="http://guolin.tech/api/weather?cityid="+weatherId+"&key=bc0418b57b2d4918819d3974ac1285d9";
         HttpUtil.sendOkHttpRequest(weatherUrl, new Callback() {
-            @Override
-            public void onFailure(Call call, IOException e) {
-             e.printStackTrace();
-             runOnUiThread(new Runnable() {
-                 @Override
-                 public void run() {
-                     Toast.makeText(WeatherActivity.this,"获取天气信息失败",Toast.LENGTH_SHORT).show();
-                 }
-             });
-            }
+        @Override
+        public void onFailure(Call call, IOException e) {
+            e.printStackTrace();
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    Toast.makeText(WeatherActivity.this,"获取天气信息失败",Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
 
-            @Override
-            public void onResponse(Call call, Response response) throws IOException {
-               final String responseText=response.body().string();
-               final Weather weather=Utility.handleWeatherResponse(responseText);
-               runOnUiThread(new Runnable() {
-                   @Override
-                   public void run() {
-                       if(weather!=null&&"ok".equals(weather.status)){
-                           SharedPreferences.Editor editor=PreferenceManager.getDefaultSharedPreferences(WeatherActivity.this).edit();
-                           editor.putString("weather",responseText);
-                           editor.apply();
-                           mWeatherId=weather.basic.weatherId;
-                           showWeatherInfo(weather);
-                       }else {
-                           Toast.makeText(WeatherActivity.this,"获取天气信息失败",Toast.LENGTH_SHORT).show();
-                       }
-                       swipeRefresh.setRefreshing(false);
-                   }
-               });
-            }
-        });
-        loadBingPic();
-    }
+        @Override
+        public void onResponse(Call call, Response response) throws IOException {
+            final String responseText=response.body().string();
+            final Weather weather=Utility.handleWeatherResponse(responseText);
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    if(weather!=null&&"ok".equals(weather.status)){
+                        SharedPreferences.Editor editor=PreferenceManager.getDefaultSharedPreferences(WeatherActivity.this).edit();
+                        editor.putString("weather",responseText);
+                        editor.apply();
+                        mWeatherId=weather.basic.weatherId;
+                        showWeatherInfo(weather);
+                    }else {
+                        Toast.makeText(WeatherActivity.this,"获取天气信息失败",Toast.LENGTH_SHORT).show();
+                    }
+                    swipeRefresh.setRefreshing(false);
+                }
+            });
+        }
+    });
+    loadBingPic();
+}
     /*
     * 加载必应每日一图*/
     private void loadBingPic(){
@@ -203,5 +205,7 @@ public class WeatherActivity extends AppCompatActivity {
         carWashText.setText(carWash);
         sportText.setText(sport);
         weatherLayout.setVisibility(View.VISIBLE);
+        Intent intent=new Intent(this, AutoUpdateService.class);
+        startService(intent);
     }
 }
